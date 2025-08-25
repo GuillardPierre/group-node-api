1ecce91f9e0b8f5d865c8f61059e8ed473aa981b
Bertrand <bertrand.tang.dev@gmail.com>
2025-08-25 16:04:19 +0200

    Add tests/app.test.js, updates development to fit constraints of the exercice

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-import express from "express"
-import bodyParser from "body-parser"
+const express = require("express");
 const app = express()
-const port = 3000
-const users = [{id: 1, name: "Jean"}, {id: 2, name:"Pierre"}]
-app.use(bodyParser.json());
-
-app.post('/users', (req, res) => {
-const newUser = req.body;
-newUser.id = users.length + 1;
-users.push(newUser);
-res.status(201).json(newUser);
-});
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World")
 });
 
-app.get("/users", (req,res) => {
-    res.json(users)
+app.get("/status", (req,res) => {
+    res.json({status: 'API is running'})
 })
 
 app.listen(port, () => {
     console.log("App running on the port " + port)
 });
 
-app.use((req, res) => {
-  res.status(404).send("Vous êtes perdus ? Page non trouvée");
-});
+module.exports = app;
