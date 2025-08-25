a86af8aa665d0c2eedf1f16a6c0dd96e3d05d3b8
Bertrand <bertrand.tang.dev@gmail.com>
2025-08-25 15:11:52 +0200

    Add /users POST route to add new user

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require("express")
+import express from "express"
+import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+const newUser = req.body;
+newUser.id = users.length + 1;
+users.push(newUser);
+res.status(201).json(newUser);
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World")
-})
+});
 
 app.listen(port, () => {
     console.log("App running on the port " + port)
-})
+});
